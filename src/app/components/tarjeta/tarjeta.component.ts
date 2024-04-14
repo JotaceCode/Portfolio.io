15a7ebb2f6a7450de0257a7fc4bfc76a459717e3
JotaceCode <josvelsei@gmail.com>
2024-04-14 16:53:17 +0200

    Added proyects-card  logic
    will show a group of three elements in a row, Iterating over the row instead of projects.

--- src/app/components/tarjeta/tarjeta.component.ts
+++ src/app/components/tarjeta/tarjeta.component.ts
@@ -32,10 +32,73 @@ export class TarjetaComponent implements OnInit {
       tecnologias: ['Laravel', 'HTML', 'CSS', 'Figma']
 
 
+    },
+    {
+      url: '../assets/images/1.png',
+      titulo: 'Gymnasio',
+      descripcion: 'Práctica con MVC',
+      tecnologias: ['Php', 'HTML', 'CSS']
+
+    },
+    {
+      url: '../assets/images/3.png',
+      titulo: 'Portfolio',
+      descripcion: 'Experimentación con Angular',
+      tecnologias: ['Angular', 'HTML', 'CSS', 'Node']
+
+
+    },
+    {
+      url: '../assets/images/login.png',
+      titulo: 'Login',
+      descripcion: 'Experimentación ASP.NET MVC',
+      tecnologias: ['Laravel', 'HTML', 'CSS', 'Figma']
+
+
+    },
+    {
+      url: '../assets/images/1.png',
+      titulo: 'Gymnasio',
+      descripcion: 'Práctica con MVC',
+      tecnologias: ['Php', 'HTML', 'CSS']
+
+    },
+    {
+      url: '../assets/images/3.png',
+      titulo: 'Portfolio',
+      descripcion: 'Experimentación con Angular',
+      tecnologias: ['Angular', 'HTML', 'CSS', 'Node']
+
+
+    },
+    {
+      url: '../assets/images/login.png',
+      titulo: 'Login',
+      descripcion: 'Experimentación ASP.NET MVC',
+      tecnologias: ['Laravel', 'HTML', 'CSS', 'Figma']
+
+
     }
   ];
   constructor() {}
+
+
+  
   ngOnInit(): void {
 
   }
+
+
+  //Método que devuelve un array de tres proyectos por fila(para mostrar 3 por row en la vista)
+  get proyectosPorFilas() {
+    const proyectos = [...this.listProyectos]; // Copiar la lista de proyectos
+    const proyectosPorFilas = [];
+  
+    while (proyectos.length > 0) {
+      proyectosPorFilas.push(proyectos.splice(0, 3)); // Extraer y añadir grupos de 3 proyectos
+    }
+  
+    return proyectosPorFilas;
+  }
+  
 }
